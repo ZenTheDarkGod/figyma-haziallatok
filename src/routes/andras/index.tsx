2f0532e34f82fc2bd052a84e4800a867649de7f8
ZenTheDarkGod <takaczsoli@gmail.com>
2023-10-05 18:39:27 +0200

    demo done

--- src/routes/andras/index.tsx
+++ src/routes/andras/index.tsx
@@ -12,7 +12,28 @@ const NAME = "András";
 
 export default component$(() => {
 
-  const content = `### Írás alatt`;
+  const content = `
+  
+  ### Rubiról
+  Rubi egy Amerikai Cocker Spániel,szuka kutya. 
+  Nagyon játékos, kiváncsi és nagyon szereti az embereket. 
+  Gyakran viszem sétálni, de a kertben is el tud játszani Cooperrel. 
+  Séta közben mindenre kiváncsi és mindenkivel játszani akar és ez össze is jön neki, mert nagyon. aranyosan néz arra, akivel játszani akar. 
+  
+  ---
+
+  ### Étkezése 
+  Naponta Kétszer1 tál ételt eszik, néha szokott étkezések közt jutalom falatot kapni. 
+  Ilyen táplálkozás mellett egészséges és erős tud maradni. 
+  
+  ---
+  
+  ### Tisztitása
+  Cooperhez hasonlóan havonta egy fürdést igényel Rubi, természetesen ha nagyon koszos akkor muszáj megfürdenie. 
+  
+  ---
+
+  ### Egyéb információk Rubi most 4éves, Oroszlányról hoztuk el, és születésnapomra kaptam.`;
 
   return (
     <>
